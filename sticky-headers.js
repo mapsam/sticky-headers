747ce1978665cebc29bb29d9caf878d64e6f0c9c
Sam Matthews <matthews.sam@gmail.com>
2013-03-17 22:43:35 -0700

    Update sticky-headers.js

--- sticky-headers.js
+++ sticky-headers.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-  var fixed = false;
+	var fixed = false;
 	var topHeight = $('#header').height();
 	var navHeight = $('#nav').height();
 	var sectionHeaderHeight = $('.title').height();
 	var sec0 = $('#section0').height() + topHeight + navHeight;
 	var sec1 = $('#section1').height() + sec0 + sectionHeaderHeight;
-	var sec2 = $('#section2').height() + sec1 + sectionHeaderHeight;
-	var sec3 = $('#section3').height() + sec2 + sectionHeaderHeight;
-	var sec4 = $('#section4').height() + sec3 + sectionHeaderHeight;
-	var sec5 = $('#section5').height() + sec4 + sectionHeaderHeight;
+	var sec2 = $(headers[1][1]).height() + sec1 + sectionHeaderHeight;
+	var sec3 = $(headers[2][1]).height() + sec2 + sectionHeaderHeight;
+	var sec4 = $(headers[3][1]).height() + sec3 + sectionHeaderHeight;
+	var sec5 = $(headers[4][1]).height() + sec4 + sectionHeaderHeight;
 	$(document).scroll(function(){
 		if($(this).scrollTop() >= topHeight) {
 			if(!fixed) {
@@ -24,44 +24,46 @@ $(document).ready(function(){
 			}
 		}
 
+		var headers = [['#sectionHeader1', '#section1'], ['#sectionHeader2', headers[1][1]], ['#sectionHeader3', headers[2][1]], ['#sectionHeader4', headers[3][1]], ['#sectionHeader5', headers[4][1]]]
+
 		if(($(this).scrollTop() >= sec0) && ($(this).scrollTop() < sec1)) {
-			$('#sectionHeader1').css({position:'fixed', top:'0px'});
-			$('#section1').css({'padding-top':sectionHeaderHeight+'px'});
+			$(headers[0][0]).css({position:'fixed', top:'0px'});
+			$(headers[0][1]).css({'padding-top':sectionHeaderHeight+'px'});
 		} else {
-			$('#sectionHeader1').css({position:'relative', top:'0px'});
-			$('#section1').css({'padding-top':0});
+			$(headers[0][0]).css({position:'relative', top:'0px'});
+			$(headers[0][1]).css({'padding-top':0});
 		}
 
 		if(($(this).scrollTop() >= sec1) && ($(this).scrollTop() < sec2)) {
-			$('#sectionHeader2').css({position:'fixed', top:'0px'});
-			$('#section2').css({'padding-top':sectionHeaderHeight+'px'});
+			$(headers[1][0]).css({position:'fixed', top:'0px'});
+			$(headers[1][1]).css({'padding-top':sectionHeaderHeight+'px'});
 		} else {
-			$('#sectionHeader2').css({position:'relative', top:'0px'});
-			$('#section2').css({'padding-top':0});
+			$(headers[1][0]).css({position:'relative', top:'0px'});
+			$(headers[1][1]).css({'padding-top':0});
 		}
 
 		if(($(this).scrollTop() >= sec2) && ($(this).scrollTop() < sec3)) {
-			$('#sectionHeader3').css({position:'fixed', top:'0px'});
-			$('#section3').css({'padding-top':sectionHeaderHeight+'px'});
+			$(headers[2][0]).css({position:'fixed', top:'0px'});
+			$(headers[2][1]).css({'padding-top':sectionHeaderHeight+'px'});
 		} else {
-			$('#sectionHeader3').css({position:'relative', top:'0px'});
-			$('#section3').css({'padding-top':0});
+			$(headers[2][0]).css({position:'relative', top:'0px'});
+			$(headers[2][1]).css({'padding-top':0});
 		}
 
 		if(($(this).scrollTop() >= sec3) && ($(this).scrollTop() < sec4)) {
-			$('#sectionHeader4').css({position:'fixed', top:'0px'});
-			$('#section4').css({'padding-top':sectionHeaderHeight+'px'});
+			$(headers[3][0]).css({position:'fixed', top:'0px'});
+			$(headers[3][1]).css({'padding-top':sectionHeaderHeight+'px'});
 		} else {
-			$('#sectionHeader4').css({position:'relative', top:'0px'});
-			$('#section4').css({'padding-top':0});
+			$(headers[3][0]).css({position:'relative', top:'0px'});
+			$(headers[3][1]).css({'padding-top':0});
 		}
 
 		if(($(this).scrollTop() >= sec4) && ($(this).scrollTop() < sec5)) {
-			$('#sectionHeader5').css({position:'fixed', top:'0px'});
-			$('#section5').css({'padding-top':sectionHeaderHeight+'px'});
+			$(headers[4][0]).css({position:'fixed', top:'0px'});
+			$(headers[4][1]).css({'padding-top':sectionHeaderHeight+'px'});
 		} else {
-			$('#sectionHeader5').css({position:'relative', top:'0px'});
-			$('#section5').css({'padding-top':0});
+			$(headers[4][0]).css({position:'relative', top:'0px'});
+			$(headers[4][1]).css({'padding-top':0});
 		}
 	});
 });
